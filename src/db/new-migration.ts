ac74e5451456d5bcfcaf71d8a33bc0bf19b00217
Hussein Ismail <hussein.ismail@eatech.fi>
2020-02-22 23:14:05 +0200

    Clean unwanted contents

--- src/db/new-migration.ts
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import fs from "fs";
-
-const prefix = new Date().getTime();
-const filePath = path.join(__dirname, "migrations", `${prefix}_rename.ts`);
-fs.writeFileSync(filePath, "");
